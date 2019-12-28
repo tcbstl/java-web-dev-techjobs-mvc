48d24a39298fecda288e4a1141e1d7b297532094
Tommy Balestreri <tcbstl@gmail.com>
2019-12-27 20:36:00 -0600

    Everything done besides having Search form on search results page

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -17,7 +17,9 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 @RequestMapping("search")
 public class SearchController {
 
-    @RequestMapping(value = "")
+//    @RequestMapping(value = "")
+//    @RequestMapping(value = {"","results"})
+    @RequestMapping()
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
@@ -25,5 +27,53 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+//    @RequestMapping(value = "results")
+//    public String searchResults(Model model) {
+//        model.addAttribute("columns", columnChoices);
+//        return "search";
+//    }
 
+    @RequestMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+
+        ArrayList<Job> jobs;
+
+        if (searchTerm == "all") {
+            jobs = JobData.findAll();
+        }
+
+        if (searchType.toLowerCase().equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+          model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+        } else if (searchType.toLowerCase().equals("employer")) {
+            jobs = JobData.findByColumnAndValue("employer", searchTerm);
+          model.addAttribute("title", "Jobs With Employer: " + searchTerm);
+//          model.addAttribute("form",)
+
+            model.addAttribute("jobs", jobs);
+        } else if (searchType.toLowerCase().equals("location")) {
+            jobs = JobData.findByColumnAndValue("location", searchTerm);
+          model.addAttribute("title", "Jobs With Location: " + searchTerm);
+            model.addAttribute("jobs", jobs);
+        } else if (searchType.toLowerCase().equals("corecompetency")) {
+            jobs = JobData.findByColumnAndValue("coreCompetency", searchTerm);
+          model.addAttribute("title", "Jobs With Skill: " + searchTerm);
+            model.addAttribute("jobs", jobs);
+        } else if (searchType.toLowerCase().equals("positiontype")) {
+            jobs = JobData.findByColumnAndValue("positionType", searchTerm);
+          model.addAttribute("title", "Jobs With PositionType :"+ searchTerm);
+
+            model.addAttribute("jobs", jobs);
+        }
+
+//      else if (searchType.toLowerCase)
+
+        else {
+            jobs = JobData.findAll();
+        }
+
+        return "list-jobs";
+
+    }
 }
