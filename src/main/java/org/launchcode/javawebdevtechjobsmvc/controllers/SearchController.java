09e66911b1ebb2440f63b8244d358115f5535071
Tommy Balestreri <tcbstl@gmail.com>
2020-01-05 13:31:54 -0600

    Everything done besides searching "all" for "all"

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -17,9 +17,9 @@ import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.co
 @RequestMapping("search")
 public class SearchController {
 
-//    @RequestMapping(value = "")
+    @RequestMapping(value = "")
 //    @RequestMapping(value = {"","results"})
-    @RequestMapping()
+//    @RequestMapping()
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
@@ -27,45 +27,54 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
-//    @RequestMapping(value = "results")
-//    public String searchResults(Model model) {
+    @RequestMapping(value = "results")
+//    public String searchHeader(Model model) {
 //        model.addAttribute("columns", columnChoices);
 //        return "search";
 //    }
-
-    @RequestMapping(value = "results")
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
-
+        model.addAttribute("columns", columnChoices);
         ArrayList<Job> jobs;
+
+
+
 
         if (searchTerm == "all") {
             jobs = JobData.findAll();
         }
 
-        if (searchType.toLowerCase().equals("all")) {
+        else if (searchType.toLowerCase().equals("all")) {
             jobs = JobData.findByValue(searchTerm);
-          model.addAttribute("title", "All Jobs");
+            model.addAttribute("title", "All Jobs");
             model.addAttribute("jobs", jobs);
+
+            //if (searchType.toLowerCase().equals("all")) {
+//            jobs = JobData.findByValue(searchTerm);
+//            model.addAttribute("title", "All Jobs");
+//            model.addAttribute("jobs", jobs);
+
         } else if (searchType.toLowerCase().equals("employer")) {
             jobs = JobData.findByColumnAndValue("employer", searchTerm);
-          model.addAttribute("title", "Jobs With Employer: " + searchTerm);
+            model.addAttribute("title", "Jobs With Employer: " + searchTerm);
 //          model.addAttribute("form",)
-
             model.addAttribute("jobs", jobs);
+
         } else if (searchType.toLowerCase().equals("location")) {
             jobs = JobData.findByColumnAndValue("location", searchTerm);
-          model.addAttribute("title", "Jobs With Location: " + searchTerm);
+            model.addAttribute("title", "Jobs With Location: " + searchTerm);
             model.addAttribute("jobs", jobs);
         } else if (searchType.toLowerCase().equals("corecompetency")) {
             jobs = JobData.findByColumnAndValue("coreCompetency", searchTerm);
-          model.addAttribute("title", "Jobs With Skill: " + searchTerm);
+            model.addAttribute("title", "Jobs With Skill: " + searchTerm);
             model.addAttribute("jobs", jobs);
+
         } else if (searchType.toLowerCase().equals("positiontype")) {
             jobs = JobData.findByColumnAndValue("positionType", searchTerm);
-          model.addAttribute("title", "Jobs With PositionType :"+ searchTerm);
-
+            model.addAttribute("title", "Jobs With PositionType :"+ searchTerm);
             model.addAttribute("jobs", jobs);
         }
+
+
 
 //      else if (searchType.toLowerCase)
 
@@ -73,7 +82,95 @@ public class SearchController {
             jobs = JobData.findAll();
         }
 
-        return "list-jobs";
-
+//        return "list-jobs";
+        return "search";
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    @RequestMapping(value = "results")
+//    public String searchResults(Model model) {
+//        model.addAttribute("columns", columnChoices);
+//        return "search";
+//    }
+
+//    @RequestMapping(value = "results")
+//    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+//
+//        ArrayList<Job> jobs;
+//
+//        if (searchTerm == "all") {
+//            jobs = JobData.findAll();
+//        }
+//
+//        if (searchType.toLowerCase().equals("all")) {
+//            jobs = JobData.findByValue(searchTerm);
+//          model.addAttribute("title", "All Jobs");
+//            model.addAttribute("jobs", jobs);
+//        } else if (searchType.toLowerCase().equals("employer")) {
+//            jobs = JobData.findByColumnAndValue("employer", searchTerm);
+//          model.addAttribute("title", "Jobs With Employer: " + searchTerm);
+////          model.addAttribute("form",)
+//
+//            model.addAttribute("jobs", jobs);
+//        } else if (searchType.toLowerCase().equals("location")) {
+//            jobs = JobData.findByColumnAndValue("location", searchTerm);
+//          model.addAttribute("title", "Jobs With Location: " + searchTerm);
+//            model.addAttribute("jobs", jobs);
+//        } else if (searchType.toLowerCase().equals("corecompetency")) {
+//            jobs = JobData.findByColumnAndValue("coreCompetency", searchTerm);
+//          model.addAttribute("title", "Jobs With Skill: " + searchTerm);
+//            model.addAttribute("jobs", jobs);
+//        } else if (searchType.toLowerCase().equals("positiontype")) {
+//            jobs = JobData.findByColumnAndValue("positionType", searchTerm);
+//          model.addAttribute("title", "Jobs With PositionType :"+ searchTerm);
+//
+//            model.addAttribute("jobs", jobs);
+//        }
+//
+////      else if (searchType.toLowerCase)
+//
+//        else {
+//            jobs = JobData.findAll();
+//        }
+//
+//        return "list-jobs";
+//
+//    }
 }
